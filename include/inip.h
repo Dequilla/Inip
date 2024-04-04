59d2b6385b4bb051c2559b1689738a4e60052bd7
Edwin Wallin <edwin.wallin@dequilla.com>
2024-04-04 17:50:54 +0200

    Initial commit.

---
+++ include/inip.h
@@ -0,0 +1,6 @@
+#ifndef __INIP_H__
+#define __INIP_H__
+
+void first_function();
+
+#endif
---
+++ src/inip.c
@@ -0,0 +1,8 @@
+#include "inip.h"
+
+#include <stdio.h>
+
+void first_function()
+{
+    printf("Hello world!");
+}
